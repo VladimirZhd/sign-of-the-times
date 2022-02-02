550b7a81648e62da2837f5649e0bd6d5633fb440
Ethan <91624529+ethancmeeker@users.noreply.github.com>
2022-02-01 21:43:13 -0700

    My stuff

--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+
+
+var searched = document.getElementById("search");
+function Search() {
+	document.getElementById("x").innerHTML = document.getElementById("searcher").value;
+}
+
+function Submission() {
+	document.getElementById("thanks").innerHTML = "Thank you, the translation has been put on the cloud";
+}
+
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
 	document.getElementById('root')
 );
+
+
