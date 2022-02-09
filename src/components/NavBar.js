7d4c7546325e115e042866173ad3adc6ac776be6
Joseph61t <83661458+Joseph61t@users.noreply.github.com>
2022-02-09 16:18:01 -0700

    added buttons for navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavBar = () => {
+	return (
+		<>	
+		
+		<ul className='nav-bar'>
+			<li><button type="button" className='Home-Button' className='button'>Home</button></li>
+			<li><button type="button" className='GIFtionary-Button' className='button'>GIFtionary</button></li>
+		</ul>
+
+		</>
+	);
+};
+
+export default NavBar;
