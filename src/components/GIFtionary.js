8daf3cc4f1a63237cc60eaaa665230f3f97a15c7
Joseph61t <83661458+Joseph61t@users.noreply.github.com>
2022-02-09 15:30:34 -0700

    Starting structure for navbar, and GIFtionary page

---
+++ src/components/GIFtionary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+const GIFtionary = () => {
+	return (
+		
+		<>	
+		<NavBar/>
+		<div className='GIFtionary-wrapper'>GIF Dictionary Page</div>
+
+		</>
+	);
+};
+
+export default GIFtionary;
