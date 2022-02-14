09569e76f259849d6633de7b02d111beecb2b986
Joseph61t <83661458+Joseph61t@users.noreply.github.com>
2022-02-14 16:09:45 -0700

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,10 +5,14 @@ const NavBar = () => {
 	return (
 		<>	
 		
+
 		<ul className='nav-bar'>
 			<li><Link to="/"><button type="button" className='Home-Button' className='button'>Home</button></Link></li>
 			<li><Link to="/giftionary"><button type="button" className='GIFtionary-Button' className='button'>GIFtionary</button></Link></li>
 		</ul>
+
+		<div className='nav-bar'>This is the header.</div>
+
 
 		</>
 	);
