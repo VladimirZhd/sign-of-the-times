5828a5b3d92022e58b7080d67776315c4c42c818
VladimirZhd <Voodhed88->
2022-03-08 10:23:27 -0700

    aaded comments

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,31 +3,37 @@ import React, { useState } from 'react';
 import { db } from '../firebase';
 
 const Home = () => {
+	// Create a state for search
 	const [data, setData] = useState({
 		searchPhrase: '',
 		result: [],
 		error: '',
 		loading: false,
 	});
+	// Extract state variables to local variables
 	const { searchPhrase, error, loading, result } = data;
-
+	// Handle searchPhrase change
 	const handleChange = (e) => {
 		setData({ ...data, searchPhrase: e.target.value });
 	};
-
+	// Handle submit
 	const handleSubmit = async (e) => {
 		try {
 			e.preventDefault();
 			setData({ ...data, loading: true });
+			// Create a query to search in the database
 			const q = query(
 				collection(db, 'gifs'),
 				where('translation', '==', searchPhrase)
 			);
+			// Get docs using query
 			const snapGif = await getDocs(q);
 			const tempGifs = [];
+			// Save docs to a temporal array
 			snapGif.docs.forEach((doc) => {
 				tempGifs.push(doc.data());
 			});
+			// Save retrieved docs to the state
 			if (tempGifs.length > 0) {
 				setData({
 					...data,
