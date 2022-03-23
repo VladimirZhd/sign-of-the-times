696a11ba188f2923ec43ec37febccdca61a44ab4
Joseph61t <83661458+Joseph61t@users.noreply.github.com>
2022-03-23 16:04:38 -0600

    general overhall for home css

--- src/components/Home.js
+++ src/components/Home.js
@@ -47,7 +47,7 @@ const Home = () => {
 			} else {
 				setData({
 					...data,
-					error: 'Not found',
+					error: '',
 					searchPhrase: '',
 					loading: false,
 					result: [],
@@ -80,28 +80,28 @@ const Home = () => {
 			{result &&
 				result.map((gif) => (
 
-// 					<div className='result-container'>
-// 						<Card className ='card'
-// 								border='secondary'
-// 								style={{ width: '18rem', margin: '1rem' }}
-// 								key={gif.uid}>
-// 								<Card.Img variant='top' src={gif.gifUrl} />
-// 								<Card.Body>
-// 									<Card.Title style={{}}>
-// 										{gif.translation}
-// 									</Card.Title>
-// 									{/* <Card.Text>
-// 						This is a longer card with supporting text below as a natural
-// 						lead-in to additional content. This content is a little bit longer.
-// 					  </Card.Text> */}
-// 								</Card.Body>
-// 							</Card>
-// 						{/* <img src={gif.gifUrl} alt={gif.translation} />
-// 						<p>{gif.translation}</p> */}
+					<div className='result-container'>
+						<Card className ='result'
+								border='secondary'
+								style={{ width: '18rem'}}
+								key={gif.uid}>
+								<Card.Img variant='top' src={gif.gifUrl} />
+								<Card.Body>
+									<Card.Title style={{}}>
+										{gif.translation}
+									</Card.Title>
+									{/* <Card.Text>
+						This is a longer card with supporting text below as a natural
+						lead-in to additional content. This content is a little bit longer.
+					  </Card.Text> */}
+								</Card.Body>
+							</Card>
+						{/* <img src={gif.gifUrl} alt={gif.translation} />
+						<p>{gif.translation}</p> */}
 
-					<div className='result-container' key={gif.uid}>
+					{/* <div className='result-container' key={gif.uid}>
 						<img src={gif.gifUrl} alt={gif.translation} />
-						<p>{gif.translation.join(' ')}</p>
+						<p>{gif.translation.join(' ')}</p> */}
 
 					</div>
 				))}
