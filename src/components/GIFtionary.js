c61c0c5d85b600585d0966743e73c040e0003a72
VladimirZhd <Voodhed88->
2022-03-28 16:12:53 -0600

    added link

--- src/components/GIFtionary.js
+++ src/components/GIFtionary.js
@@ -4,6 +4,7 @@ import { db } from '../firebase';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
 
 const GIFtionary = () => {
 	const [gifs, setGifs] = useState([]);
@@ -24,8 +25,14 @@ const GIFtionary = () => {
 					<div className='giftionary-wrapper'></div>
 					{gifs.length > 0 &&
 						gifs.map((gif) => (
-							<Link to={`/gif/${gif.uid}`}
-							style={{ width: '18rem', margin: '1rem' }}>
+							<Link
+								to={`/gif/${gif.uid}`}
+								style={{
+									width: '18rem',
+									margin: '1rem',
+									color: 'black',
+									textDecoration: 'none',
+								}}>
 								<Card
 									className='gif'
 									border='secondary'
@@ -35,13 +42,13 @@ const GIFtionary = () => {
 										variant='top'
 										src={gif.gifUrl}
 									/>
-									<Card.Img
-										className='topimg'
-										variant='top'
-										src={
-											'https://docs.google.com/drawings/d/e/2PACX-1vQOf72qNFE4S83F8AOFk32tGyzu7qVIoiv8kYfITU3N8YgYMhSZ69-Z2Gnk6buaPu9krqjziqiM9Va0/pub?w=689&h=690'
-										}
-									/>
+									{gif.imageUrl && (
+										<Card.Img
+											className='topimg'
+											variant='top'
+											src={gif.imageUrl}
+										/>
+									)}
 									<Card.Body>
 										<Card.Title style={{}}>
 											{gif.translation.join(' ')}
